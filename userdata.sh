4270e83351b899e9306ff01d9dc37faa8a46be1b
surya prasad bandari <suryaprasadbandari@gmail.com>
2026-03-22 21:14:58 +0530

    modified files

--- userdata.sh
+++ userdata.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
 
-# Replace {YOUR_GIT_REOPO_URL} with your actual Git repository URL
-#GIT_REPO_URL="https://github.com/codewithmuh/django-aws-ec2-autoscaling.git"
-#git@github.com:suryaprasad1/django-project-loadbalancer-autoscaling.git
-##GIT_REPO_URL="https://suryaprasad1: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@github.com/codewithmuh/django-aws-ec2-autoscaling.git"
-GIT_REPO_URL="https://suryaprasad1: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git@github.com:suryaprasad1/django-project-loadbalancer-autoscaling.git"
+#GIT_REPO_URL="https://<your_username>:<your_PAT>git@github.com:suryaprasad1/django-project-loadbalancer-autoscaling.git"
 
 # Replace {YOUR_PROJECT_MAIN_DIR_NAME} with your actual project directory name
 PROJECT_MAIN_DIR_NAME="django-aws-ec2-autoscaling"
